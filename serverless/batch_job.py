abb2502eddcdfb1d6e24ad01b7e47a5109dd439c
Aaron Falloon <aaron.falloon@bazaarvoice.com>
2019-03-08 18:16:36 +0000

    Start to split transferring from FTP and extracting

--- serverless/batch_job.py
+++ serverless/batch_job.py
@@ -16,7 +16,10 @@ s3 = boto3.client('s3')
 s3_raw_bucket = f'{os.environ["INITIALS"]}-cca-ted-raw-{os.environ["STAGE"]}'
 s3_extracted_bucket = f'{os.environ["INITIALS"]}-cca-ted-extracted-{os.environ["STAGE"]}'
 
-def start(event, context):
+# TODO Handler errors and exceptions
+
+def start_transfer_job(event, context):
+    logger.info('Starting transfer job')
     dir_to_walk = 'daily-packages/'
     if 'year' in event and 'month' in event:
         dir_to_walk = f'{dir_to_walk}{event["year"]}/{event["month"]}/'
@@ -26,17 +29,50 @@ def start(event, context):
         for path in fs.walk.files(filter=['*.tar.gz']):
             # TODO Batch and send
             sqs.send_message(
-                QueueUrl=f'https://sqs.eu-west-3.amazonaws.com/{os.environ["AWS_ACCOUNT_ID"]}/{os.environ["INITIALS"]}_cca_ted_batch_job_{os.environ["STAGE"]}',
+                QueueUrl=f'https://sqs.eu-west-3.amazonaws.com/{os.environ["AWS_ACCOUNT_ID"]}/{os.environ["INITIALS"]}_cca_ted_transfers_{os.environ["STAGE"]}',
                 MessageBody=json.dumps({
                     'path': dir_to_walk + path[1:]
                 })
             )
     logger.info('Finished walking %s', dir_to_walk)
+    logger.info('Finished starting transfer job')
     return {
         'statusCode': 200
     }
 
-def process_batch(event, context):
+def process_transfers(event, context):
+    logger.info('Processing transfers')
+    paths = [json.loads(record['body'])['path'] for record in event['Records']]
+    _transfer_files_from_ftp_to_s3(paths)
+    logger.info('Finished processing transfers')
+    return {
+        'statusCode': 200
+    }
+
+def _transfer_files_from_ftp_to_s3(paths):
+    with open_fs('ftp://guest:guest@ted.europa.eu/') as ftp_fs:
+        for path in paths:
+            # TODO Don't do if already in S3
+            tmp_path = f'/tmp/{os.path.split(path)[1]}'
+            with open(tmp_path, 'wb') as tmp_file:
+                logger.info('Downloading %s to %s', path, tmp_path)
+                ftp_fs.download(path, tmp_file)
+                logger.info('Finished downloading %s to %s', path, tmp_path)
+            s3_key = path.replace('daily-packages/', '')
+            full_s3_path = f'{s3_raw_bucket}/{s3_key}'
+            logger.info('Uploading %s to %s', tmp_path, full_s3_path)
+            s3.upload_file(
+                tmp_path,
+                s3_raw_bucket,
+                s3_key
+            )
+            logger.info('Finished uploading %s to %s', tmp_path, full_s3_path)
+            os.remove(tmp_path)
+
+def start_extraction_job(event, context):
+    pass
+
+def process_extractions(event, context):
     paths = [json.loads(record['body'])['path'] for record in event['Records']]
     _transfer_files_from_ftp_to_s3(paths)
     logger.info('Downloading files from S3')
@@ -61,26 +97,6 @@ def process_batch(event, context):
     return {
         'statusCode': 200
     }
-
-def _transfer_files_from_ftp_to_s3(paths):
-    with open_fs('ftp://guest:guest@ted.europa.eu/') as ftp_fs:
-        for path in paths:
-            # TODO Don't do if already in S3
-            tmp_path = f'/tmp/{os.path.split(path)[1]}'
-            with open(tmp_path, 'wb') as tmp_file:
-                logger.info('Downloading %s to %s', path, tmp_path)
-                ftp_fs.download(path, tmp_file)
-                logger.info('Finished downloading %s to %s', path, tmp_path)
-            s3_key = path.replace('daily-packages/', '')
-            full_s3_path = f'{s3_raw_bucket}/{s3_key}'
-            logger.info('Uploading %s to %s', tmp_path, full_s3_path)
-            s3.upload_file(
-                tmp_path,
-                s3_raw_bucket,
-                s3_key
-            )
-            logger.info('Finished uploading %s to %s', tmp_path, full_s3_path)
-            os.remove(tmp_path)
 
 def _download_files_from_s3(paths):
     downloaded_file_paths = []
--- terraform/sqs/main.tf
+++ terraform/sqs/main.tf
@@ -1,4 +1,9 @@
-resource "aws_sqs_queue" "batch_job" {
-    name = "${var.initials}_cca_ted_batch_job_${var.stage}"
+resource "aws_sqs_queue" "transfers" {
+    name = "${var.initials}_cca_ted_transfers_${var.stage}"
+    visibility_timeout_seconds = 900
+}
+
+resource "aws_sqs_queue" "extractions" {
+    name = "${var.initials}_cca_ted_extractions_${var.stage}"
     visibility_timeout_seconds = 900
 }
